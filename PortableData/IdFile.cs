a5c73d3b733de01c1b6d3437bf770ac367e72fb7
aldente <aldente@hirosaki-u.ac.jp>
2017-02-08 10:51:19 +0900

    とりあえずPortableDataを実装。

---
+++ PortableData/IdFile.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace HirosakiUniversity.Aldente.AES.Data.Portable
+{
+
+	public class IdFile
+	{
+
+		public static DataType CheckType(string source)
+		{
+			using (var reader = new StreamReader(new FileStream(source, FileMode.Open, FileAccess.Read)))
+			{
+				while (!reader.EndOfStream)
+				{
+					var cols = reader.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+					if (cols.Length > 1)
+					{
+						switch (cols[0])
+						{
+							case "$AP_DATATYPE":
+								switch (Convert.ToInt32(cols[1]))
+								{
+									case 3:
+										return DataType.WideScan;
+									case 5:
+										return DataType.DepthProfile;
+									default:
+										throw new Exception("この測定形式には対応していません。");
+								}
+						}
+					}
+				}
+			}
+			throw new Exception("このデータファイルは対応していません。");
+		}
+
+		public static async Task<DataType> CheckTypeAsync(string source)
+		{
+			using (var reader = new StreamReader(new FileStream(source, FileMode.Open, FileAccess.Read)))
+			{
+				while (!reader.EndOfStream)
+				{
+					var line = await reader.ReadLineAsync();
+					var cols = line.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+					if (cols.Length > 1)
+					{
+						switch (cols[0])
+						{
+							case "$AP_DATATYPE":
+								switch (Convert.ToInt32(cols[1]))
+								{
+									case 3:
+										return DataType.WideScan;
+									case 5:
+										return DataType.DepthProfile;
+									default:
+										throw new Exception("この測定形式には対応していません。");
+								}
+						}
+					}
+				}
+			}
+			throw new Exception("このデータファイルは対応していません。");
+		}
+
+	}
+
+	#region DataType列挙体
+	public enum DataType
+	{
+		SEImage,
+		WideScan,
+		SplitScan,
+		DepthProfile,
+		LineProfile,
+		SpectrumLineProfile,
+		Mapping,
+		LargeAreaMapping
+	}
+	#endregion
+
+}
---
+++ PortableData/WideScan.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+
+namespace HirosakiUniversity.Aldente.AES.Data.Portable
+{
+
+	#region WideScanクラス
+	public class WideScan
+	{
+
+		#region *Parameterプロパティ
+		public ScanParameter Parameter
+		{
+			get
+			{
+				return _scanParameter;
+			}
+		}
+		ScanParameter _scanParameter;
+		#endregion
+
+		#region *Dataプロパティ
+		public EqualIntervalData Data
+		{
+			get
+			{
+				return _data;
+			}
+		}
+		EqualIntervalData _data;
+		#endregion
+
+		private WideScan()
+		{ }
+
+		public static async Task<WideScan> GenerateAsync(string directory)
+		{
+			var scan = new WideScan();
+
+			// パラメータを読み込む。
+			scan._scanParameter = await ScanParameter.GenerateAsync(directory);
+
+			// データを読み込む。
+			using (var reader = new BinaryReader(new FileStream(Path.Combine(directory, "data"), FileMode.Open, FileAccess.Read)))
+			{
+				scan._data = await EqualIntervalData.GenerateAsync(reader);
+			}
+
+			return scan;
+		}
+
+
+
+		/// <summary>
+		/// 範囲を制限したデータを返します。
+		/// </summary>
+		/// <param name="start"></param>
+		/// <param name="stop"></param>
+		/// <returns></returns>
+		public WideScan GetRestrictedData(decimal start, decimal stop)
+		{
+			var b = Convert.ToInt32(Decimal.Floor((start - Parameter.Start) / Parameter.Step));
+			var e = Convert.ToInt32(Decimal.Ceiling((stop - Parameter.Start) / Parameter.Step));
+
+			return new WideScan
+			{
+				_data = this.Data.GetSubData(b, e),
+				_scanParameter = this.Parameter.ShrinkRange(b, e)
+			};
+		}
+
+
+		/// <summary>
+		/// 前後のデータから線形補間した値を返します。
+		/// </summary>
+		/// <param name="x"></param>
+		/// <returns></returns>
+		public decimal GetInterpolatedDataAt(decimal x)
+		{
+			if (x >= Parameter.Start && x <= Parameter.Stop)
+			{
+				var index = (x - Parameter.Start) / Parameter.Step;
+				int i = Convert.ToInt32(Decimal.Floor(index));
+				decimal d = index - i;
+
+				return (1 - d) * Data[i] + d * Data[i + 1];
+			}
+			else
+			{
+				throw new ArgumentException();
+			}
+		}
+
+		// ※XShiftを考慮するのはここだけかな？
+
+		/// <summary>
+		/// 指定した範囲の、線形補間されたデータを返します。XShiftを考慮します。
+		/// </summary>
+		/// <param name="start"></param>
+		/// <param name="step"></param>
+		/// <param name="count"></param>
+		/// <returns></returns>
+		public List<decimal> GetInterpolatedData(decimal start, decimal step, int count)
+		{
+			List<decimal> data = new List<decimal>();
+			for (int i = 0; i < count; i++)
+			{
+				data.Add(GetInterpolatedDataAt(start + step * i - _scanParameter.XShift));
+			}
+			return data;
+		}
+
+
+
+		public WideScan Differentiate(int m)
+		{
+			return new WideScan
+			{
+				_scanParameter = this._scanParameter.GetDifferentiatedParameter(m),
+				_data = this.Data.Differentiate(m, _scanParameter.Step)
+			};
+		}
+
+		#region *csvとしてエクスポート(ExportCsv)
+		public void ExportCsv(TextWriter writer)
+		{
+			for (int i = 0; i < Parameter.PointsCount; i++)
+			{
+				decimal x = Parameter.Start + i * Parameter.Step;
+				writer.WriteLine($"{x},{Data.GetDataForCsv(i)}");
+			}
+		}
+		#endregion
+
+	}
+	#endregion
+
+
+}
