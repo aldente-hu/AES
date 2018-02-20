a7e4a1942134aafd34147bcfc820315d6876b8e7
aldente <aldente@hirosaki-u.ac.jp>
2018-02-20 16:04:22 +0900

    新プロジェクトを追加して、行列計算ライブラリを更新。

---
+++ OtameshiCoreConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HirosakiUniversity.Aldente.AES.OtameshiCoreConsole
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
